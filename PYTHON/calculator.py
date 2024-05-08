6dc199f984161c98986dc0d0cae9e18c6fa83b18
Rita Ogada <88630122+ritaogada@users.noreply.github.com>
2024-05-08 16:45:21 +0300

    defining functions in python

--- PYTHON/calculator.py
+++ PYTHON/calculator.py
@@ -1,5 +1,8 @@
-x = float(input("what's x? "))
-y = float(input("what's y? "))
-
-z = x / y
-print(f"{z:.2f}")
+def main():
+    x = int(input("what's x? "))
+    print("x squared is", square(x))
+    
+def square(n):
+    return pow(n, 2)
+    
+main()
--- PYTHON/hello.py
+++ PYTHON/hello.py
@@ -1,8 +1,8 @@
-# Ask user for their name
-name = input("What's your name? ").strip().title()
+def main():
+    name = input("What's your name? ")
+    hello(name)
 
-# Split user's name into first name and last name
-first, last = name.split(" ")
-
-# Greet user
-print(f"hello, {first}")
+def hello(to="world"):
+    print("hello,", to)
+    
+main()
