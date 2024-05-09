5f84fa461a51893911c10f71b238bef178ba33b2
Rita Ogada <88630122+ritaogada@users.noreply.github.com>
2024-05-09 12:19:33 +0300

    conditionals in python

---
+++ compare.py
@@ -0,0 +1,10 @@
+x = int(input("what's x? "))
+y = int(input("what's y? "))
+
+if x < y:
+    print("x is less than y")  
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
+
