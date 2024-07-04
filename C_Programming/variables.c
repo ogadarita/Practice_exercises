102e3545e0628b2abeb45e7487c06167db52f578
Rita Ogada <88630122+ritaogada@users.noreply.github.com>
2024-07-04 11:49:38 +0300

    variables

---
+++ C_Programming/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int position = 10;
+	int x, y, z;
+	x = 2;
+	y = 3;
+	z = 4;
+
+	printf("position: %d\n", position);
+	printf("x: %d, y: %d, z: %d\n", x, y, z);
+
+	return (0);
+}
